669134236e86c6899fb7196c9c0d6a2cfe08303b
Агулов Владимир <vo.agul.va@gmail.com>
2021-11-27 14:03:33 +0700

    Add CMake

---
+++ PersistentDataStructures/PersistentDataStructures.cpp
@@ -0,0 +1,9 @@
+﻿// PersistentDataStructures.cpp: определяет точку входа для приложения.
+//
+
+#include "PersistentDataStructures.h"
+
+const char* getHelloWorld() {
+	return "Hello, world!";
+}
+
---
+++ PersistentDataStructures/main.cpp
@@ -0,0 +1,9 @@
+#include "PersistentDataStructures.h"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << getHelloWorld() << std::endl;
+	return 0;
+}
